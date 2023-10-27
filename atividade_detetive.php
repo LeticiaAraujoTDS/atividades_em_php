9c2d8c8d8724b69a07d7edb5bce97921d393e09b
Leticia <134729304+LeticiaAraujoTDS@users.noreply.github.com>
2023-10-27 14:40:41 -0300

    Update atividade_detetive.php

--- atividade_detetive.php
+++ atividade_detetive.php
@@ -24,4 +24,30 @@
             print"Assasino\n";
     }
 
+//jeito php
+
+<?php
+
+    $suspeito = 0;
+    $respostas = [];
+
+    $respostas[] = readline("Telefonou para a vítima? \n");
+    $respostas[] = readline("Esteve no local do crime? \n");
+    $respostas[] = readline("Mora perto da vítima? \n");
+    $respostas[] = readline("Devia para a vítima? \n");
+    $respostas[] = readline("Já trabalhou com a vítima? \n");
+
+    $suspeito = array_count_values($respostas);
+
+    if ($suspeito ['sim'] <= 2) {
+        print"Inocente\n";
+    } else if ($suspeito ['sim'] == 4 || $suspeito ['sim'] == 3) {
+        print"Cumplice\n";
+    } else if ($suspeito ['sim'] == 5) {
+            print"Assasino\n";
+    }
+
+
+
+
 
